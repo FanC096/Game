a5418db51333bf306a3abce082b933c6707b9c06
FanC096 <qianfan@LAPTOP-40ISVAKD.localdomain>
2018-11-26 14:58:16 -0500

    templates

--- playgame.ml
+++
@@ -1,8 +0,0 @@
-#use "nim.ml" ;;
-#use "ai_nim_player.ml" ;;
-#use "referee.ml" ;;
-#use "human_player.ml" ;;
-
-module HumanAIReferee = Referee(Nim15) (HumanPlayer(Nim15)) (AINimPlayer) ;;
-open HumanAIReferee ;;
-play_game();;
--- referee.ml
+++ referee.ml
@@ -1,7 +1,9 @@
 #use "sig_game.ml" ;;
 #use "sig_player.ml" ;;
 
-module Referee = functor (Game : GAME) -> functor (Player1 : PLAYER with module PlayerGame = Game) -> functor (Player2 : PLAYER with module PlayerGame = Game) ->
+module Referee = functor (Game : GAME) -> 
+                functor (Player1: PLAYER with module PlayerGame = Game) -> 
+                functor (Player2: PLAYER with module PlayerGame = Game) ->
 struct
   module CurrentGame = Game
 
@@ -14,13 +16,12 @@ struct
       | CurrentGame.Ongoing player ->
         print_endline ((CurrentGame.string_of_player player) ^ "'s turn.") ;
         let the_move = (match player with
-                      Game.P1 -> Player1.next_move s
-                    | Game.P2 -> Player2.next_move s) in
+                      CurrentGame.P1 -> Player1.next_move s
+                    | CurrentGame.P2 -> Player2.next_move s) in
         print_endline ((CurrentGame.string_of_player player) ^
                         " makes the move " ^
                         (CurrentGame.string_of_move the_move)) ;
-        game_loop (CurrentGame.next_state (s, the_move))
+        game_loop (CurrentGame.next_state s the_move)
     in try game_loop CurrentGame.initial_state with
        | Failure message -> print_endline message
-
 end ;;
